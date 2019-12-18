a8e382249523d3b07ec16813ab2dddfe2ed97158
prudiusDenys <prdslife@gmail.com>
2019-12-18 15:18:13 +0100

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -14,7 +14,36 @@ $(function(){
       slidesToScroll: 4,
 
     });
-var mixer = mixitup('.products__inner-box');
+
+  $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: "$"
+  });
+
+  $('.icon-th-list').on('click', function(){
+    $('.products__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+  $('.icon-th-large').on('click', function(){
+    $('.products__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+  });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+    $('.header__btn-menu').on('click', function (){
+      $('.header__box').toggleClass('active');
+    });
+
+    var mixer = mixitup('.products__inner-box');
 
 
   
